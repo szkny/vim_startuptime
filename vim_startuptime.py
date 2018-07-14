b8a7d8158416769e5e0bf301c83aa8278361e8ef
szkny <suzuki.s1008@gmail.com>
2018-07-14 18:28:20 +0900

    2018年 7月14日 土曜日 18時28分20秒 JST

--- vim_startuptime.py
+++ vim_startuptime.py
@@ -5,6 +5,7 @@ Created on Sat Jul 7th 00:49:17 2018
     @file  : vim_startuptime.py
     @author: Suzuki
     @brief : vim startup time analyzer.
+             [USAGE] python vim_startuptime.py {VIM_ARGS}
 """
 from modules.vim_performance import VimPerformance
 import sys
