6b9b0cba2159740bb68be6aa3da037e336a5e902
szkny <suzuki.s1008@gmail.com>
2018-07-20 07:21:05 +0900

    2018年 7月20日 金曜日 07時21分05秒 JST

--- vim_startuptime.py
+++ vim_startuptime.py
@@ -8,7 +8,7 @@ Created on Sat Jul 7th 00:49:17 2018
              [USAGE] python vim_startuptime.py {VIM_ARGS}
 """
 from modules.vim_performance import VimPerformance
-import sys
+from sys import argv
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
@@ -18,12 +18,11 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 
 if __name__ == '__main__':
-    Argv = sys.argv[1:]
     """ VimPerformance's param: vim='vim'(default) or if $EDITOR (environment variable) exists, use it.
     """
     obj = VimPerformance(vim='nvim')
     obj.clean()
-    obj.measure(100, vim_args=Argv)
+    obj.measure(100, vim_args=argv[1:])
     """ plot's param: kind='pie'(default) or 'hist' or 'line'
     """
     obj.plot(kind='pie')
