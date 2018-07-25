070df93f2cb67c2708d52e86727fabb5c15afc8a
szkny <suzuki.s1008@gmail.com>
2018-07-26 07:02:47 +0900

    2018年 7月26日 木曜日 07時02分47秒 JST

--- vim_startuptime.py
+++ vim_startuptime.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 7th 00:49:17 2018
+Created on Tue Jul 24th 22:46:56 2018
     @file  : vim_startuptime.py
-    @author: Suzuki
+    @author: suzukisohei
     @brief : vim startup time analyzer.
              [USAGE] python vim_startuptime.py {VIM_ARGS}
 """
